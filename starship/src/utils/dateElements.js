76acc8847bd04d855a54981d77e39bbc22b3fb6a
Nabil <nabilaltamash@gmail.com>
2021-09-14 23:35:06 +0600

    Fixed a date issue, criticalgit status !

--- starship/src/utils/dateElements.js
+++ starship/src/utils/dateElements.js
@@ -1,20 +1,47 @@
-let DATES = [];
-for (let i = 1; i <= 31; i++) {
-  DATES.push(i);
-}
-
 export const months = [
-  { name: 'January', value: 1 },
-  { name: 'February', value: 2 },
-  { name: 'March', value: 3 },
-  { name: 'April', value: 4 },
-  { name: 'May', value: 5 },
-  { name: 'June', value: 6 },
-  { name: 'July', value: 7 },
-  { name: 'August', value: 8 },
-  { name: 'September', value: 9 },
-  { name: 'October', value: 10 },
-  { name: 'November', value: 11 },
-  { name: 'December', value: 12 }
+  { name: 'January', value: '01' },
+  { name: 'February', value: '02' },
+  { name: 'March', value: '03' },
+  { name: 'April', value: '04' },
+  { name: 'May', value: '05' },
+  { name: 'June', value: '06' },
+  { name: 'July', value: '07' },
+  { name: 'August', value: '08' },
+  { name: 'September', value: '09' },
+  { name: 'October', value: '10' },
+  { name: 'November', value: '11' },
+  { name: 'December', value: '12' }
 ];
-export const dates = [...DATES];
+export const dates = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+  '24',
+  '25',
+  '26',
+  '27',
+  '28',
+  '29',
+  '30',
+  '31'
+];
